2b4d029a47bdd87a1f3da016ea6c5e2f35935e15
jia joestar <emilyshaven@outlook.com>
2025-02-04 12:54:37 +0000

    lab2 revision + lab3 stuff

--- lab2-functionalprimer/haskell/task2.hs
+++ lab2-functionalprimer/haskell/task2.hs
@@ -4,4 +4,4 @@ main = do
     putStrLn "Welcome to the programme. Please enter your name"
     name <- getLine
     putStrLn("Hello " ++ name ++ ", hope you like Haskell.")
-    print (onePlusOne)
+    print(onePlusOne)
--- lab2-functionalprimer/python3/func_as_arg.py
+++ lab2-functionalprimer/python3/func_as_arg.py
@@ -1,10 +1,10 @@
 def listFunc():
-    return [i for i in range(1, 6)] #Create list of ints from 1 to 5, Haskell equivalent [1..5]
+    return [i for i in range(a, b)] #Create list of ints from a to b, Haskell equivalent [a..b]
 
 def applicatorFunc(inpFunc, s):
     if s=='s':
         return sum(inpFunc())
     else:
-        return sum(inpFunc())/5
+        return sum(inpFunc())/(b-a+1)
 
-print(applicatorFunc(listFunc, 's'))
+print(applicatorFunc(listFunc, 'a', 1, 10))
